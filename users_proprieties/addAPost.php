ac58fe908ec3f07d79350eea7cb5398fa306fb6c
Coxtin <olteancostin3@gmail.com>
2023-05-04 19:07:55 +0300

    Update addAPost.php

--- users_proprieties/addAPost.php
+++ users_proprieties/addAPost.php
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 <?php require_once("../config.php"); ?>
 <?php include("../includes_signup_login/code_functions.php");?>
 <?php checkIfUserIsConnected(); ?>
 
 <?php include("../includes/sidebar.php") ?>
-=======
 <?php include("../includes/sidebar.php") ?>
 <?php include("../config.php"); ?>
 <link rel="stylesheet" href="../static/css/addAPost.css">
@@ -15,4 +13,3 @@
     <input type="submit" name = "submit" value="Upload">
     </form>
 </div>
->>>>>>> ae462c348e1366d7a8d922629a0ed8202169eb70
